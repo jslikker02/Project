4f2bba9b89017e4505be97e9f62f3d29cc32b4d8
JAY-JAY SLIKKER <jay-jayslikker@MacBook-Pro-van-JAY-JAY.local>
2017-11-09 10:00:14 +0100

    aanpassing herkansing

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,26 +1,47 @@
 var button = document.querySelector('section button');
 
 button.addEventListener('click', function(){
+         if(this.classList.toggle('button.active')){
+         counter++;
+     }else{
+     counter--;}
+document.querySelector("header div").innerHTML = counter;
    button.classList.toggle('active')
 
 });
 var download = document.querySelector('.download');
 
 download.addEventListener('click', function(){
-   download.classList.toggle('active')
+       if(this.classList.toggle('download2.active')){
+         counter++;
+     }else{
+     counter--;}
+document.querySelector("header p").innerHTML = counter;
+   this.classList.toggle('active');
     });
 
 var download2 = document.querySelectorAll('.download2');
 for (var i= 0; i < download2.length; i++) {
 download2[i].addEventListener('click', function(){
-
+     if(this.classList.toggle('download2.active')){
+         counter++;
+     }else{
+     counter--;}
+document.querySelector("header p").innerHTML = counter;
    this.classList.toggle('active');
 })}
 
+
+
+var counter = 0;
 var button2 = document.querySelectorAll('main article button');
 for (var i= 0; i < button2.length; i++) {
 button2[i].addEventListener('click', function(){
-
+     if(this.classList.toggle('button.active')){
+         counter++;
+     }else{
+     counter--;}
+document.querySelector("header div").innerHTML = counter;
    this.classList.toggle('active');
 })}
 
